5bd9cc62980141078ccd65177725611c8c233134
Faisal <khanfasial.makent@gmail.com>
2026-03-02 14:18:15 +0530

    config improve

--- includes/config/config.php
+++ includes/config/config.php
@@ -1,6 +1,7 @@
 <?php
+
 // Central base URL for the whole website (must end with a trailing slash)
-$defaultBaseUrl = 'http://localhost/chinese/';
+$defaultBaseUrl = $websiteURL || '/';
 if (!defined('BASE_URL')) {
 	define('BASE_URL', $defaultBaseUrl);
 }
