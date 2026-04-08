37a61bc36072a857275bf4f870bb270ff224cca4
umair <umair.makent@gmail.com>
2026-04-08 19:54:29 +0530

    #1 add phone to usermanage

--- cms2/edituser.php
+++ cms2/edituser.php
@@ -1,5 +1,4 @@
-<?PHP 
-
+<?PHP
 ob_start();
 session_start();
 
@@ -9,73 +8,133 @@ session_start();
 	include ("../includes/common/inc/sessionheader.php");
 	include("../includes/common/classes/classUser.php");
 
-	$objDBcon    = new classDbConnection; 			// VALIDATION CLASS OBJECT
+	$objDBcon    = new classDbConnection;
 	$objValid 	 = new Validate_fields;
-	$objUser	 = new classUser($objDBcon);	// VALIDATION CLASS classCategory
-	$strError	=	"";
-//-------------------------------------------------------------------------------------------
+	$objUser	 = new classUser($objDBcon);
+	$strError	 = "";
+	$formNoticeClass = 'user-form-notice is-error';
+
 include_once 'functions.php';
 $ccAdminData = get_session_data();
 
-		$spram	=   array();
-
-if (isset($_POST['submit']) && $_POST['submit']	==	"submit")
-
+if(!function_exists('admin_user_normalize_phone'))
+{
+	function admin_user_normalize_phone($value)
 	{
-		   $spram[0]	= $_GET['nid'];
-		   $spram[1]    = $_POST['fname'];
-		   $spram[2]	= $_POST['surname'];
-		   $spram[5]	= $_POST['email'];
-		   $spram[9]	= $_POST['passuser'];
-		   $spram[10]	=  $_POST['user_status'];
-		  
-//------------------------------------------validation--------------------------------//			
-		$objValid->add_text_field("Last Name", $_POST['surname'], "text", "y");
-		$objValid->add_text_field("First Name", $_POST['fname'], "text", "y");
-//-----------------------------------------------------------------------------------//
+		$value = trim($value);
+		return preg_replace('/\s+/', ' ', $value);
+	}
+}
 
-		if (!$objValid->validation()) 
+$spram = array();
+$spram[0] = "";
+$spram[1] = "";
+$spram[2] = "";
+$spram[5] = "";
+$spram[9] = "";
+$spram[10] = "Active";
+$spram[13] = "";
 
-			$strError = $objValid->create_msg();
-			if($_POST['passuser'] == "" && $_POST['user_cpassword'] == "")
-			{
-			$spram[9]		 =  $_POST['user_hpassword'];
-			}elseif(isset($_POST['passuser']) and  $_POST['passuser'] != '' && $_POST['passuser'] != $_POST['user_cpassword'])
-			$strError	.=	"The field Password and Confirm Password are not same.";
-			
-		if(empty($strError))
+$nid = (isset($_GET['nid']) && ctype_digit((string)$_GET['nid'])) ? $_GET['nid'] : '';
+$returnParams = array();
+$returnKeys = array('criteria', 'textSearch', 'txt_StartDate', 'txt_StartDate2', 'currentPage', 'pgIndex');
+foreach($returnKeys as $returnKey)
+{
+	if(isset($_GET[$returnKey]) && $_GET[$returnKey] !== '')
+	{
+		$returnParams[$returnKey] = $_GET[$returnKey];
+	}
+}
+$backToListUrl = 'manageuser.php';
+if(!empty($returnParams))
+{
+	$backToListUrl .= '?'.http_build_query($returnParams);
+}
+
+$existingUser = false;
+if($nid != '')
+{
+	$resultSet = $objUser->getUser($nid);
+	$existingUser = mysql_fetch_array($resultSet);
+	if($existingUser)
+	{
+		$spram[0] = $nid;
+		$spram[1] = $existingUser['user_fname'];
+		$spram[2] = $existingUser['user_lname'];
+		$spram[5] = $existingUser['user_email'];
+		$spram[9] = $existingUser['user_password'];
+		$spram[10] = $existingUser['user_status'];
+		$spram[13] = $existingUser['user_phone'];
+	}
+	else
+	{
+		$strError = "The selected user could not be found.";
+	}
+}
+else
+{
+	$strError = "Invalid user selected.";
+}
+
+if(isset($_POST['submit']) && $_POST['submit'] == "submit" && $existingUser)
+{
+	$spram[0]	= $nid;
+	$spram[1]    = trim($_POST['fname']);
+	$spram[2]	= trim($_POST['surname']);
+	$spram[5]	= $existingUser['user_email'];
+	$spram[9]	= isset($_POST['passuser']) ? $_POST['passuser'] : '';
+	$spram[10]	= isset($_POST['user_status']) ? trim($_POST['user_status']) : 'Active';
+	$spram[13]	= admin_user_normalize_phone(isset($_POST['user_phone']) ? $_POST['user_phone'] : '');
+
+	$objValid->add_text_field("Last Name", $spram[2], "text", "y");
+	$objValid->add_text_field("First Name", $spram[1], "text", "y");
+
+	if (!$objValid->validation())
+	{
+		$strError = $objValid->create_msg();
+	}
+
+	if($spram[13] == '')
+	{
+		$strError	.=	"<b>The field Phone is empty.</b>";
+	}
+	elseif(strlen($spram[13]) > 255)
+	{
+		$strError	.=	"<b>The field Phone must be 255 characters or fewer.</b>";
+	}
+	elseif(!preg_match('/^[0-9+\-\(\)\s\.]+$/', $spram[13]))
+	{
+		$strError	.=	"<b>The field Phone contains invalid characters.</b>";
+	}
+
+	if($_POST['passuser'] == "" && $_POST['user_cpassword'] == "")
+	{
+		$spram[9] = $_POST['user_hpassword'];
+	}
+	elseif($_POST['passuser'] != $_POST['user_cpassword'])
+	{
+		$strError	.=	"<b>The field Password and Confirm Password are not same.</b>";
+	}
+
+	if(empty($strError))
+	{
+		$objUser->UpdateUserAdmin($spram);
+		$strError = "User has been Updated successfully";
+		$formNoticeClass = 'user-form-notice is-success';
+
+		$resultSet = $objUser->getUser($nid);
+		$existingUser = mysql_fetch_array($resultSet);
+		if($existingUser)
 		{
-				if($_POST['email']!=''){
-				//$spram[5] 	= $_POST['email'];
-				}
-				else{
-				//$spram[5] 	= $_POST['hiddenemail'];
-				}
-			$objUser->UpdateUserAdmin($spram);
-			$strError	= "User has been Updated successfully";
-	    }
-
-	   }
-			$spram[0]	    =	$_GET['nid'];
-			
-			
-			$resultSet		 =	$objUser->getUser($spram[0]);
-			
-			while($row1		 =	mysql_fetch_array($resultSet))
-			{
-		 	$spram[1]		 =  $row1['user_fname'];
-		 	$spram[2]		 =  $row1['user_lname'];
-			$spram[5]		 =  $row1['user_email'];
-			$spram[9]		 =  $row1['user_password'];
-			$spram[10]		 =  $row1['user_status'];
-			
-			}
-//--------------------------------------------------------------------------------------//
-
-/*Html View Area*/
+			$spram[1] = $existingUser['user_fname'];
+			$spram[2] = $existingUser['user_lname'];
+			$spram[5] = $existingUser['user_email'];
+			$spram[9] = $existingUser['user_password'];
+			$spram[10] = $existingUser['user_status'];
+			$spram[13] = $existingUser['user_phone'];
+		}
+	}
+}
 
 include "html/edituser.html";
-
-//--------------------------------------------------------------------------------------//
-
 ?>
--- cms2/manageuser.php
+++ cms2/manageuser.php
@@ -8,136 +8,277 @@ session_start();
 	include("../includes/common/classes/classUser.php");
 	include("../includes/common/classes/classPagingAdmin.php");
 	include("../functions.php");
-//---------------------------------------------------------------
-	$objDBcon    = new classDbConnection; 			// VALIDATION CLASS OBJECT
-	$objUser	 = new classUser($objDBcon);	// VALIDATION CLASS classCategory
-	
+
+	$objDBcon    = new classDbConnection;
+	$objUser	 = new classUser($objDBcon);
+
 include_once 'functions.php';
 $ccAdminData = get_session_data();
-//-----------------------------------------------------------------
-//----------------------------------------------------------------------------------------------------	
-	$show				=	"";
-	$title				=	"";
-	$resultset			=	"";
-	$condition			=	"";
-	$limist				=	"";
-	$LIMIT				=	"";
-	$TotalRecs	     	= 	0;	
-	$NaviLinks        	= 	"";	
-	$BackNaviLinks	 	= 	"";		
-	$ForwardNaviLinks 	= 	"";		
-	$TotalPages	      	= 	"";
-	$PageNo		      	= 	1;
-	$PageIndex	      	= 	1 ;				
-	if(isset($_POST['Search'])){
-	$rowsPerPage      	= 	1000000000;
-	}else{
-	$rowsPerPage      	= 	100;
-	}														
-	$linkPerPage      	= 	25;
-	$of					=	"of";
-//----------------------------------------------------------------------------------------------------		
 
-		if(!isset($pgObj) && empty($pgObj))
-				$pgObj 		=  new classPaging ("manageuser.php",$rowsPerPage,$linkPerPage,"","","");
-		
-		if(isset($_GET['pgIndex']) && !empty($_GET['pgIndex']) && is_numeric($_GET['pgIndex']) && is_numeric($_GET['currentPage']))
-				{
-					$PageNo			 = $_GET['currentPage'];	# Shows # of Page			
-					$PageIndex		 = $_GET['pgIndex'];		# Shows # of Page Index				
-				}	
-
-					$LIMIT .= " LIMIT " . (($PageNo-1) * $rowsPerPage). " , " . $rowsPerPage;
-//----------------------------------------------------------------------------------------------------	
-    if(isset($_GET['action']) && $_GET['action']	== "up" )
+function manageuser_normalize_date($value)
+{
+	$value = trim($value);
+	if($value == '')
 	{
+		return '';
+	}
+
+	if(!preg_match('/^\d{4}-\d{2}-\d{2}$/', $value))
+	{
+		return '';
+	}
+
+	list($year, $month, $day) = explode('-', $value);
+	if(!checkdate((int)$month, (int)$day, (int)$year))
+	{
+		return '';
+	}
+
+	return $value;
+}
+
+function manageuser_build_query($params)
+{
+	$cleanParams = array();
+	foreach($params as $key => $value)
+	{
+		if($value === '' || $value === null)
+		{
+			continue;
+		}
+		$cleanParams[$key] = $value;
+	}
+
+	return http_build_query($cleanParams);
+}
+
+function manageuser_redirect($params = array())
+{
+	$url = 'manageuser.php';
+	$query = manageuser_build_query($params);
+	if($query != '')
+	{
+		$url .= '?'.$query;
+	}
+	header('Location: '.$url);
+	exit;
+}
+
+$show				=	"";
+$LIMIT				=	"";
+$TotalRecs	     	= 	0;
+$NaviLinks        	= 	"";
+$BackNaviLinks	 	= 	"";
+$ForwardNaviLinks 	= 	"";
+$TotalPages	      	= 	"";
+$PageNo		      	= 	1;
+$PageIndex	      	= 	1;
+$rowsPerPage      	= 	100;
+$linkPerPage      	= 	25;
+$noticeMessages	=	array();
+$noticeClass		=	'user-alert is-info';
+
+$allowedCriteria = array(
+	'Order_Id'		=> 'Transaction Id',
+	'user_email'	=> 'Email Address',
+	'user_fname'	=> 'First Name',
+	'user_lname'	=> 'Last Name',
+	'user_phone'	=> 'Phone Number'
+);
+
+	if(isset($_GET['pgIndex']) && ctype_digit((string)$_GET['pgIndex']) && (int)$_GET['pgIndex'] > 0)
+	{
+		$PageIndex = (int)$_GET['pgIndex'];
+	}
+
+	if(isset($_GET['currentPage']) && ctype_digit((string)$_GET['currentPage']) && (int)$_GET['currentPage'] > 0)
+	{
+		$PageNo = (int)$_GET['currentPage'];
+	}
+
+$LIMIT = " LIMIT " . (($PageNo-1) * $rowsPerPage). " , " . $rowsPerPage;
+
+$criteria = (isset($_GET['criteria']) && isset($allowedCriteria[$_GET['criteria']])) ? $_GET['criteria'] : 'user_email';
+$textSearch = isset($_GET['textSearch']) ? trim($_GET['textSearch']) : '';
+$textSearch = preg_replace('/\s+/', ' ', $textSearch);
+
+$rawStartDate = isset($_GET['txt_StartDate']) ? trim($_GET['txt_StartDate']) : '';
+$rawEndDate = isset($_GET['txt_StartDate2']) ? trim($_GET['txt_StartDate2']) : '';
+$txt_StartDate = manageuser_normalize_date($rawStartDate);
+$txt_StartDate2 = manageuser_normalize_date($rawEndDate);
+
+if($rawStartDate != '' && $txt_StartDate == '')
+{
+	$noticeMessages[] = 'The start date filter was ignored because it was not in YYYY-MM-DD format.';
+	$noticeClass = 'user-alert is-warning';
+}
+
+if($rawEndDate != '' && $txt_StartDate2 == '')
+{
+	$noticeMessages[] = 'The end date filter was ignored because it was not in YYYY-MM-DD format.';
+	$noticeClass = 'user-alert is-warning';
+}
+
+$filterParams = array();
+if(isset($_GET['criteria']) || $textSearch != '')
+{
+	$filterParams['criteria'] = $criteria;
+}
+if($textSearch != '')
+{
+	$filterParams['textSearch'] = $textSearch;
+}
+if($txt_StartDate != '')
+{
+	$filterParams['txt_StartDate'] = $txt_StartDate;
+}
+if($txt_StartDate2 != '')
+{
+	$filterParams['txt_StartDate2'] = $txt_StartDate2;
+}
+
+$pagingQueryString = manageuser_build_query($filterParams);
+$pagingQuerySuffix = ($pagingQueryString != '') ? '&'.$pagingQueryString : '';
+
+$stateParams = $filterParams;
+if($PageNo > 1)
+{
+	$stateParams['currentPage'] = $PageNo;
+}
+if($PageIndex > 1)
+{
+	$stateParams['pgIndex'] = $PageIndex;
+}
+
+$stateQueryString = manageuser_build_query($stateParams);
+$searchActive = ($textSearch != '' || $txt_StartDate != '' || $txt_StartDate2 != '');
+
+if(isset($_POST['btn_delete']) && $_POST['btn_delete'] == 'Delete Selected Items')
+{
+	$counter = isset($_POST['counter']) ? (int)$_POST['counter'] : 0;
+	for($i=1; $i<=$counter; $i++)
+	{
+		if(isset($_POST['chkbox'.$i]) && ctype_digit((string)$_POST['chkbox'.$i]))
+		{
+			$objUser->deleteUser($_POST['chkbox'.$i]);
+		}
+	}
+
+	$redirectParams = array();
+	if(isset($_POST['return_query']) && $_POST['return_query'] != '')
+	{
+		parse_str($_POST['return_query'], $redirectParams);
+	}
+	if(!is_array($redirectParams))
+	{
+		$redirectParams = array();
+	}
+	unset($redirectParams['action'], $redirectParams['nid'], $redirectParams['mainsend'], $redirectParams['user'], $redirectParams['msg']);
+	$redirectParams['msg'] = 'bulk_deleted';
+	manageuser_redirect($redirectParams);
+}
+
+$actionRedirectParams = $stateParams;
+
+if(isset($_GET['action']) && $_GET['action'] == 'up' && isset($_GET['nid']) && ctype_digit((string)$_GET['nid']))
+{
 	$objUser->deleteUser($_GET['nid']);
-	}
+	$actionRedirectParams['msg'] = 'deleted';
+	manageuser_redirect($actionRedirectParams);
+}
 
-	if(isset($_POST['btn_delete']) &&	$_POST['btn_delete']	== "Delete Selected Items" )
-	{		
-			$counter	=	$_POST['counter']; 
-			for($i=1; $i<=$counter; $i++)
-				{ 
-					
-					if(isset($_POST['chkbox'.$i])){
-					$objUser->deleteUser($_POST['chkbox'.$i]);
-					}
-				}
-	}
-	
-	if(isset($_GET['mainsend']) && $_GET['mainsend']=='send'){
-		   $getuser	=	mysql_fetch_array(mysql_query("select * from tbl_user where user_id='".$_GET['user']."'"));
-		   $spram[1]	= $getuser['user_fname'];
-		   $spram[2]	= $getuser['user_lname'];
-		   $spram[5]	= $getuser['user_email'];
-		   		
-		signupemail($spram,$from);
-		$str22	=	"Email has been successfully sent.";
-		
-	}
-	
-	if(isset($_POST['Search']))
+if(isset($_GET['mainsend']) && $_GET['mainsend'] == 'send' && isset($_GET['user']) && ctype_digit((string)$_GET['user']))
+{
+	$userId = (int)$_GET['user'];
+	$getuser = mysql_fetch_array(mysql_query("select * from tbl_user where user_id='".$userId."'"));
+	if($getuser)
 	{
-		if($_POST['textSearch']!=''){
-			if($_POST['txt_StartDate']=='' && $_POST['txt_StartDate2']==''){
-				$cri	=	$_POST['criteria']." LIKE '%".$_POST['textSearch']."%' ";
-			}else{
-				$cri	=	$_POST['criteria']." LIKE '%".$_POST['textSearch']."%' and ";
-				}
-			}else{
-			$cri	=	'';
-			}
-		if($_POST['txt_StartDate']!=''){
-		if($_POST['txt_StartDate2']!=''){
-		$stDate	=	" creatDate>='".$_POST['txt_StartDate']."' and ";
-		}else{
-		$stDate	=	"creatDate>='".$_POST['txt_StartDate']."'";
-		}}else{
-		$stDate	=	'';
-		}
-		
-		if($_POST['txt_StartDate2']!=''){
-		$enDate	=	"creatDate<='".$_POST['txt_StartDate2']."'";
-		}else{
-		$enDate	=	'';
-		}
-		if(isset($_POST['criteria']) && $_POST['criteria']=='Order_Id'){
-		$que	=	"SELECT * FROM tbl_user INNER JOIN order_master ON (order_master.Cust_ID=tbl_user.user_id) WHERE order_master.Order_Id LIKE '%".$_POST['textSearch']."%'";
-		$result1	=	mysql_query($que);
-		$pgObj->SetNavigationalLinksNew($result1);
-		$result= mysql_query($que);
-		}else{		
-		$result1	=	mysql_query("select * from tbl_user where ".$cri." ".$stDate." ".$enDate." order by creatDate DESC".$limist);
-		$pgObj->SetNavigationalLinksNew($result1);
-		$result= mysql_query("select * from tbl_user where ".$cri." ".$stDate." ".$enDate." order by creatDate DESC".$LIMIT);
-		}
-	
-	}else{
-	
-		$qry = "select * from  tbl_user order by `user_id` DESC".$limist;
-		$result1 = mysql_query($qry);
-		$pgObj->SetNavigationalLinksNew($result1);
-		$qry2 = "select * from  tbl_user order by `user_id` DESC".$LIMIT;
-		$result = mysql_query($qry2);
-	}
-	
-	
-//............................................
-	
-//............................................
+		$spram = array();
+		$spram[1] = $getuser['user_fname'];
+		$spram[2] = $getuser['user_lname'];
+		$spram[5] = $getuser['user_email'];
+		$spram[13] = $getuser['user_phone'];
 
-	if(mysql_num_rows($result) < 1){
-	$emptyError	=	"No record exist.";
-	}else{
-	$emptyError	=	'<input type="submit" name="btn_delete" id="btn_delete" class="button" value="Delete Selected Items" onclick="return Cofirm();"  />';
+		signupemail($spram, $fromEmail);
 	}
-	$show		 =	$objUser->getUserhtml($result);
+	$actionRedirectParams['msg'] = 'email_sent';
+	manageuser_redirect($actionRedirectParams);
+}
 
-//------------------------html---------------------------------------
+if(isset($_GET['msg']))
+{
+	if($_GET['msg'] == 'deleted')
+	{
+		$noticeMessages[] = 'User has been deleted successfully.';
+		$noticeClass = 'user-alert is-success';
+	}
+	elseif($_GET['msg'] == 'bulk_deleted')
+	{
+		$noticeMessages[] = 'Selected users have been deleted successfully.';
+		$noticeClass = 'user-alert is-success';
+	}
+	elseif($_GET['msg'] == 'email_sent')
+	{
+		$noticeMessages[] = 'Email has been successfully sent.';
+		$noticeClass = 'user-alert is-success';
+	}
+}
+
+$pgObj = new classPaging("manageuser.php", $rowsPerPage, $linkPerPage, $pagingQuerySuffix, "", "", "", "", "", "", "", "");
+
+$whereClauses = array();
+$escapedSearch = ($textSearch != '') ? mysql_real_escape_string($textSearch) : '';
+$queryBase = " FROM tbl_user";
+$selectClause = "SELECT tbl_user.*";
+
+if($criteria == 'Order_Id' && $textSearch != '')
+{
+	$queryBase = " FROM tbl_user INNER JOIN order_master ON (order_master.Cust_ID=tbl_user.user_id)";
+	$selectClause = "SELECT DISTINCT tbl_user.*";
+	$whereClauses[] = "order_master.Order_Id LIKE '%".$escapedSearch."%'";
+}
+elseif($textSearch != '')
+{
+	$whereClauses[] = "tbl_user.".$criteria." LIKE '%".$escapedSearch."%'";
+}
+
+if($txt_StartDate != '')
+{
+	$whereClauses[] = "tbl_user.creatDate >= '".$txt_StartDate."'";
+}
+
+if($txt_StartDate2 != '')
+{
+	$whereClauses[] = "tbl_user.creatDate <= '".$txt_StartDate2."'";
+}
+
+$whereSql = '';
+if(!empty($whereClauses))
+{
+	$whereSql = " WHERE ".implode(' AND ', $whereClauses);
+}
+
+$listQuery = $selectClause.$queryBase.$whereSql." ORDER BY tbl_user.user_id DESC";
+$result1 = mysql_query($listQuery);
+$pgObj->SetNavigationalLinksNew($result1);
+$result = mysql_query($listQuery.$LIMIT);
+
+$recordsOnPage = mysql_num_rows($result);
+$hasRecords = ($recordsOnPage > 0);
+
+if(!$hasRecords)
+{
+	$emptyMessage = $searchActive ? 'No users matched the current filters.' : 'No user record exists.';
+}
+else
+{
+	$emptyMessage = '';
+}
+
+$show = $objUser->getUserhtml($result, '', $stateQueryString);
+$pageNotice = !empty($noticeMessages) ? implode('<br />', $noticeMessages) : '';
+$clearFiltersUrl = 'manageuser.php';
+$addUserUrl = 'adduser.php';
 
 include("html/manageuser.html");
-
-//------------------------html---------------------------------------
-
 ?>
